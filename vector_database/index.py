a74a6d16108edcd160efa422ac62fb31b6b06f1f
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-08-27 17:22:00 -0600

    refactor: move Pinecone initialization to vector_database module and update index creation functions to return IndexModel

--- vector_database/index.py
+++ vector_database/index.py
@@ -3,22 +3,22 @@ Written by Juan Pablo Gutierrez
 """
 
 from vector_database import pc
-from pinecone.db_control.models import ServerlessSpec
+from pinecone.db_control.models import ServerlessSpec, IndexModel
 from pinecone.db_control.enums import CloudProvider, AwsRegion, Metric, VectorType
 from pinecone.inference import EmbedModel
-from typing import Dict
+from typing import Dict, Optional
 
 """
 Create a index with integrated embeddings.
 
 The index type (dense or sparse) is determined by the model.
 """
-def create_index_integrated_embeddings(index_name: str, cloud: CloudProvider, region: AwsRegion, model: EmbedModel, metric: Metric, field_map: Dict[str, str]):  
+def create_index_integrated_embeddings(index_name: str, cloud: CloudProvider, region: AwsRegion, model: EmbedModel, metric: Metric, field_map: Dict[str, str]) -> Optional[IndexModel]:  
     """
     Create a dense index with Pinecone.
     """
     if not pc.has_index(index_name):
-        pc.create_index_for_model(
+        res = pc.create_index_for_model(
             name=index_name,
             cloud=cloud,
             region=region,
@@ -28,16 +28,23 @@ def create_index_integrated_embeddings(index_name: str, cloud: CloudProvider, re
                 "field_map": field_map,
             },
         )
+        return res
+    
+    return None
 
-def create_index(index_name: str, dimension: int, metric: Metric, vector_type: VectorType, spec: ServerlessSpec):
+
+def create_index(index_name: str, dimension: int, metric: Metric, vector_type: VectorType, spec: ServerlessSpec) -> Optional[IndexModel]:
     """
     Create an index from a backup.
     """
     if not pc.has_index(index_name):
-        pc.create_index(
+        res = pc.create_index(
             name=index_name,
             dimension=dimension,
             metric=metric,
             vector_type=vector_type,
             spec=spec,
         )
+        return res
+    
+    return None
