10abb403bb325da97282ea23c5b7ade094bb1f03
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-08-30 20:27:33 -0600

    feat: implement embedding pipeline with OpenAI integration in embeddings/__init__.py

---
+++ embeddings/__init__.py
@@ -0,0 +1,29 @@
+"""
+Written by Juan Pablo Gutierrez
+"""
+
+from llama_index.embeddings.openai.base import OpenAIEmbedding, OpenAIEmbeddingModelType
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.node_parser import SemanticSplitterNodeParser 
+from llama_index.core.vector_stores.simple import BasePydanticVectorStore
+import os
+
+# Embed model should be defined in the config files (TODO: add later)
+embed_model = OpenAIEmbedding(api_key=os.getenv("OPENAI_API_KEY"), model=OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002)
+
+def get_pipeline(vector_store: BasePydanticVectorStore) -> IngestionPipeline:
+    """
+    Get the ingestion pipeline. Using a vector store to store the nodes.
+    """
+    return IngestionPipeline(
+        transformations=[
+            SemanticSplitterNodeParser(
+                buffer_size=1,
+                breakpoint_percentile_threshold=95, 
+                include_metadata=True,
+                embed_model=embed_model,
+            ),
+            embed_model
+        ],
+        vector_store=vector_store
+    )
